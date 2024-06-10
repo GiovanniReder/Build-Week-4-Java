d11eadb00bf35ad3ce5112d7b28f5b432cbc1250
Giovanni <gio.reder@gmail.com>
2024-06-10 12:57:56 +0200

    persistence

--- src/main/java/giovanni/Application.java
+++ src/main/java/giovanni/Application.java
@@ -1,7 +1,11 @@
 package giovanni;
 
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
 public class Application {
 
+        private static final EntityManagerFactory emf= Persistence.createEntityManagerFactory("Trasporto_pubblico");
     public static void main(String[] args) {
         System.out.println("Hello World!");
     }
